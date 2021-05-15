62dc4ff1b937ff4e3f278b8d537ae04f87a0b984
mateusvale <mateusvalexandre@gmail.com>
2021-05-14 22:39:38 -0300

    faltando apenas a ultima funcao

--- trabalho_p2/p2/trabalho_parte1.c
+++ trabalho_p2/p2/trabalho_parte1.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define TRUE 1
+#define FALSE 0
 
-void printMatriz (int *campo, int mx, int my);
+void printMatriz (int *campo, int mx, int my); //funciona
 
 void movimentacao (int *campo, int x, int y, char direcao, int mx, int my);
 
-void verifica_movimentacao (int *campo, int x, int y, char direcao, int mx, int my);
+int verifica_movimentacao (int *campo, int x, int y, char direcao, int mx, int my); //funciona
 
-void quantpecas_localidade (int *quantLocal, int *campo, int peca, int mx, int my); 
+int *quantpecas_localidade (int *campo, int peca, int mx, int my); //funciona
 
 int main(){
     int x = 7, y = 6; 
@@ -21,25 +23,29 @@ int main(){
     matrizA[3][0] = '*', matrizA[3][1] = 'c', matrizA[3][2] = 'd', matrizA[3][3] = 'd', matrizA[3][4] = 'e', matrizA[3][5] = '*';
     matrizA[4][0] = '*', matrizA[4][1] = 'c', matrizA[4][2] = 'g', matrizA[4][3] = 'h', matrizA[4][4] = 'e', matrizA[4][5] = '*';
     matrizA[5][0] = '*', matrizA[5][1] = 'f', matrizA[5][2] = ' ', matrizA[5][3] = ' ', matrizA[5][4] = 'i', matrizA[5][5] = '*';
-    matrizA[6][0] = '*', matrizA[6][1] = '*', matrizA[6][2] = ' ', matrizA[6][3] = ' ', matrizA[6][4] = '*', matrizA[6][5] = '*'; 
+    matrizA[6][0] = '*', matrizA[6][1] = '*', matrizA[6][2] = '+', matrizA[6][3] = '+', matrizA[6][4] = '*', matrizA[6][5] = '*'; 
 
     campo = &matrizA[0][0];
 
+
+    //########teste função printMatriz########//
     printMatriz (campo,x,y);
+    //########teste função printMatriz########//
+    
+    //########teste função verifica_movimentacao########//
+    printf("\nPosso movimentar? %d\n",verifica_movimentacao (campo, 2, 1, 'T', x, y));
+    //########teste função verifica_movimentacao########//
 
-    //verificar todas as letras e fazer a transformação para sexdecimal
-    int aux = 1000;
-    int *peca = &aux;
+    //########teste função quantpecas_localidade########//
+    // int *peca;
+    // peca = quantpecas_localidade(campo, ' ', x, y); 
 
-    quantpecas_localidade(peca,campo, 'a', x, y); 
-
-    printf("#############\n");//aqui
-
-    printf("%d\n",*peca);
-    printf("%d\n",*(peca+1));
-    printf("%d\n",*(peca+2));
-    printf("%d\n",*(peca+3));
-    printf("%d\n",*(peca+4));
+    // printf("%d\n",*peca);
+    // printf("%d\n",*(peca+1));
+    // printf("%d\n",*(peca+2));
+    // printf("%d\n",*(peca+3));
+    // printf("%d\n",*(peca+4));
+    //########teste função quantpecas_localidade########//
 
 }
 
@@ -53,41 +59,111 @@ void printMatriz (int *campo, int mx, int my){
     }
 };
 
-void quantpecas_localidade (int *quantLocal, int *campo, int peca, int mx, int my){
-    int total = mx*my, i = 0, count = 0, arr1[5];//, *ponteiro;
+int *quantpecas_localidade (int *campo, int peca, int mx, int my){
+    int total = mx*my, i = 0, count = 0, *count_e_localidade;
+    static int arr1[30]; //numero alto para ser maior do que a quantidade de '*'
     for (;i <= total; i++){
         if (*(campo+i) == peca){
-            arr1[count++] = i;//sexdecimal(i); //passar para sexdecimal 
+            arr1[count++] = i;
         }
     }
-    printf("#############\n");//aqui
     arr1[count] = 's';
-    int arr2[count+1];
+    static int arr2[30];
     arr2[0] = count;
     for (int j = 0;arr1[j] != 's'; j++){
         arr2[j+1] = arr1[j];
     }
-    quantLocal = arr2;
-
-    
-    // ponteiro = arr2;
-
-    printf("%d\n",*quantLocal);
-    printf("%d\n",*(quantLocal+1));
-    printf("%d\n",*(quantLocal+2));
-    printf("%d\n",*(quantLocal+3));
-    printf("%d\n",*(quantLocal+4));
-
-    //return &arr2[0];
+    count_e_localidade = arr2;
+    return count_e_localidade;
 }
 
 
-void verifica_movimentacao (int *p, int x, int y, char direcao, int mx, int my){
+int verifica_movimentacao (int *campo, int x, int y, char direcao, int mx, int my){
     int local_peca = my*x + y;
-    if (*(p+local_peca) == ' ' || *(p+local_peca) == '*'){
-        printf("Movimento Inválido\n");
-    } else{
+
+    printf("local da peça = %d\n",local_peca);
+
+    int peca = *(campo+local_peca);
+
+    printf("peça = %c\n",peca);
+
+    int *count_e_localidade;
+    count_e_localidade = quantpecas_localidade(campo, peca, mx, my); //(count,x,x,x,x)
+
+    printf("%d\n",*count_e_localidade);
+    printf("%d\n",*(count_e_localidade+1));
+
+    int quantidade = *count_e_localidade;
+    int peca_vazia = 0, peca_mesma = 0;
+    //Para o movimento da peca for valido, quando houver a movimentacao,
+    //metade de seu corpo irá para o vazio e a outra metade irá para o lugar que era sua peca 
+    //a afirmacao acima é só quando a peca tem 4 ou 2 tamanhos, pois caso seja 1 tamanho, ele vai direto para o vazio
+    //T B E D
+
+    if (peca != ' ' || peca != '*' || peca != '+'){
+        if (direcao == 'D'){
+            for (int i = 0; i < quantidade; i++){
+                if ( *(campo+( *(count_e_localidade+1+i)+1 )) == ' '){ //soma a peca em 1
+                    peca_vazia++;
+                }
+                else if (*(campo+( *(count_e_localidade+1+i)+1 )) == peca){
+                    peca_mesma++;
+                }
+            }
+
+        }
+        else if (direcao == 'E'){
+            for (int i = 0; i < quantidade; i++){
+                if ( *(campo+( *(count_e_localidade+1+i)-1 )) == ' '){ //soma a peca em 1
+                    peca_vazia++;
+                }
+                else if (*(campo+( *(count_e_localidade+1+i)-1 )) == peca){
+                    peca_mesma++;
+                }
+            }
+            
+        }
+        else if (direcao == 'B'){
+            for (int i = 0; i < quantidade; i++){
+                if ( *(campo+( *(count_e_localidade+1+i)+my )) == ' '){ //soma a peca em 1
+                    peca_vazia++;
+                }
+                else if (*(campo+( *(count_e_localidade+1+i)+my )) == peca){
+                    peca_mesma++;
+                }
+            }
+
+
+        } else if(direcao == 'T'){
+            for (int i = 0; i < quantidade; i++){
+                if ( *(campo+( *(count_e_localidade+1+i)-my )) == ' '){ //soma a peca em 1
+                    peca_vazia++;
+                }
+                else if (*(campo+( *(count_e_localidade+1+i)-my )) == peca){
+                    peca_mesma++;
+                }
+            }
+
+        } else return FALSE;
 
     }
-}
+        else{
+            return FALSE;
+            // printf("Movimento Inválido\n");
+        }
+    printf("quantidade = %d\n",quantidade);
+    printf("peca_vazia = %d\n",peca_vazia);
+    printf("peca_mesma = %d\n",peca_mesma);
 
+    if (quantidade == 4 && peca_vazia == 2 && peca_mesma ==2){
+        return TRUE;
+    }
+    else if ((quantidade == 2 && peca_vazia == 1 && peca_mesma == 1) || (quantidade == 2 && peca_vazia == 2 && peca_mesma == 0)){
+        return TRUE;
+    }
+    else if (quantidade == 1 && peca_vazia == 1 && peca_mesma ==0){
+        return TRUE;
+
+    } else return FALSE;
+
+}
---
+++ trabalho_p2/p2/trabalho_parte1_old.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void printMatriz (int *campo, int mx, int my);
+
+void movimentacao (int *campo, int x, int y, char direcao, int mx, int my);
+
+void verifica_movimentacao (int *campo, int x, int y, char direcao, int mx, int my);
+
+void quantpecas_localidade (int *quantLocal, int *campo, int peca, int mx, int my); 
+
+int main(){
+    int x = 7, y = 6; 
+
+    int matrizA[x][y], *campo;
+
+    matrizA[0][0] = '*', matrizA[0][1] = '*', matrizA[0][2] = '*', matrizA[0][3] = '*', matrizA[0][4] = '*', matrizA[0][5] = '*';
+    matrizA[1][0] = '*', matrizA[1][1] = 'a', matrizA[1][2] = 'D', matrizA[1][3] = 'D', matrizA[1][4] = 'b', matrizA[1][5] = '*';
+    matrizA[2][0] = '*', matrizA[2][1] = 'a', matrizA[2][2] = 'D', matrizA[2][3] = 'D', matrizA[2][4] = 'b', matrizA[2][5] = '*';
+    matrizA[3][0] = '*', matrizA[3][1] = 'c', matrizA[3][2] = 'd', matrizA[3][3] = 'd', matrizA[3][4] = 'e', matrizA[3][5] = '*';
+    matrizA[4][0] = '*', matrizA[4][1] = 'c', matrizA[4][2] = 'g', matrizA[4][3] = 'h', matrizA[4][4] = 'e', matrizA[4][5] = '*';
+    matrizA[5][0] = '*', matrizA[5][1] = 'f', matrizA[5][2] = ' ', matrizA[5][3] = ' ', matrizA[5][4] = 'i', matrizA[5][5] = '*';
+    matrizA[6][0] = '*', matrizA[6][1] = '*', matrizA[6][2] = ' ', matrizA[6][3] = ' ', matrizA[6][4] = '*', matrizA[6][5] = '*'; 
+
+    campo = &matrizA[0][0];
+
+    printMatriz (campo,x,y);
+
+    //verificar todas as letras e fazer a transformação para sexdecimal
+    int aux = 1000;
+    int *peca = &aux;
+
+    quantpecas_localidade(peca,campo, 'a', x, y); 
+
+    printf("#############\n");//aqui
+
+    printf("%d\n",*peca);
+    printf("%d\n",*(peca+1));
+    printf("%d\n",*(peca+2));
+    printf("%d\n",*(peca+3));
+    printf("%d\n",*(peca+4));
+
+}
+
+void printMatriz (int *campo, int mx, int my){
+    int i, j;
+    for (i = 0; i < mx; i++){
+        for (j = 0; j < my; j++){
+            printf("%c ",*(campo+j+(i*my)));
+        }
+        printf("\n");
+    }
+};
+
+void quantpecas_localidade (int *quantLocal, int *campo, int peca, int mx, int my){
+    int total = mx*my, i = 0, count = 0, arr1[5];//, *ponteiro;
+    for (;i <= total; i++){
+        if (*(campo+i) == peca){
+            arr1[count++] = i;//sexdecimal(i); //passar para sexdecimal 
+        }
+    }
+    printf("#############\n");//aqui
+    arr1[count] = 's';
+    int arr2[count+1];
+    arr2[0] = count;
+    for (int j = 0;arr1[j] != 's'; j++){
+        arr2[j+1] = arr1[j];
+    }
+    quantLocal = arr2;
+
+    
+    // ponteiro = arr2;
+
+    printf("%d\n",*quantLocal);
+    printf("%d\n",*(quantLocal+1));
+    printf("%d\n",*(quantLocal+2));
+    printf("%d\n",*(quantLocal+3));
+    printf("%d\n",*(quantLocal+4));
+
+    //return &arr2[0];
+}
+
+
+void verifica_movimentacao (int *p, int x, int y, char direcao, int mx, int my){
+    int local_peca = my*x + y;
+    if (*(p+local_peca) == ' ' || *(p+local_peca) == '*'){
+        printf("Movimento Inválido\n");
+    } else{
+
+    }
+}
+
