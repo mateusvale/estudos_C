a21e16df352e4b3fe998064d47b720c2597f0ca9
mateusvale <mateusvalexandre@gmail.com>
2021-05-19 01:14:05 -0300

    adicionando leitura na branch teste

--- trabalho_p2/p2/etapa2/leitura/etapa2_leitura_arquivo.c
+++ trabalho_p2/p2/etapa2/leitura/etapa2_leitura_arquivo.c
@@ -3,62 +3,63 @@
 #define TRUE 1
 #define FALSE 0
 
-int main(void)
-{
+
+int *configuracao (char nome_arquivo[], int config);
+
+int main(void){
+
+  char name[] = "teste.txt";  
+  int config = 1;
+
+  int *pointer = configuracao (name, config);
+
+  for (int i = 0; *(pointer+i) != '\n'; i++) printf("%c",*(pointer+i));
+
+  printf("\n");
+
+  return 0;
+}
+
+
+
+int *configuracao (char nome_arquivo[], int config){
+
   FILE *pont_arq;
-  int count, contador, c, config;
-  count = contador = 0;
-  config = 1;
+  int count = 0, contador = 0, c;
   int flag_wall = FALSE;
-  char array[100];
+  static int array[100];
   
-  //o numero de * é o mesmo - 20 para o 1 e 24 para o 2
-  //42 caracteres no 1 e 56 no 2
-  //contando com os pula linha, temos no primeiro 62 e no segundo
       
-  pont_arq = fopen("teste.txt","r");
+  pont_arq = fopen(nome_arquivo,"r");
   
-  //Faça 
   do
   {  
       c = fgetc(pont_arq);
-
       if (config == 1){
-        if (c == '*'){
-          flag_wall = TRUE;
-        }
-        if (flag_wall == TRUE && count < 42 && c != '\n'){
-          // printf("valor de c: %c\ncount: %d\n", c,count);
-          array[count++] = c;
-        }
+        if (c == '*') flag_wall = TRUE;
+        if (flag_wall == TRUE && count < 42 && c != '\n') array[count++] = c;
       }
       else{
-        if (c == '*' && contador > 20){
-          flag_wall = TRUE;
-        }
-        if (flag_wall == TRUE && count < 55 && c != '\n'){
-          // printf("valor de c: %c\ncount: %d\n", c,count);
-          array[count++] = c;
-        }
+        if (c == '*' && contador > 20) flag_wall = TRUE;
+        if (flag_wall == TRUE && count < 55 && c != '\n') array[count++] = c;
       }
-
       if (c =='*')contador++;
-      
   }while (c != EOF);
   
   fclose(pont_arq);//fechando o arquivo
 
   array[count] = '\0';
 
-  // printf("%d\n", count);
+  int *p = array;
 
   int i= 0;
   for (; array[i] != '\0'; i++){
       printf("%c",array[i]);
   }
-  // printf("\ni: %d\n",i);
-
   printf("\n");
 
-  return 0;
+  return p;
+
+  
+
 }
