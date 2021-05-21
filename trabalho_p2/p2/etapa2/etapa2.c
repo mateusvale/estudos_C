d9fee1ee415560823b2279ab202f0820c9a285fd
mateusvale <mateusvalexandre@gmail.com>
2021-05-20 23:34:33 -0300

    leitura e nome_txt no arquivo principal - etapa2.c

--- trabalho_p2/p2/etapa2/etapa2.c
+++ trabalho_p2/p2/etapa2/etapa2.c
@@ -4,22 +4,33 @@
 #define TRUE 1
 #define FALSE 0
 
-char *nome_txt(int argc, char *argv[]);
+char *nome_txt(int argc, char *argv[]); //funcionando
+
+int *leitura (char *nome_arquivo, int config); //funcionando
 
 int main (int argc, char *argv[]){
-   
+
+    //######################coleta do nome no parametro######################//
     char *nome_arquivo;
-
     nome_arquivo = nome_txt(argc,argv);
-   
-    for (int i= 0; *(nome_arquivo+i) != '\0'; i++){
-        printf("%c",*(nome_arquivo+i));
-    }
-    printf("\n");
+    // for (int i= 0; *(nome_arquivo+i) != '\0'; i++)printf("%c",*(nome_arquivo+i));
+    // printf("\n");
+    //######################coleta do nome no parametro######################//
 
+
+    //######################coleta do local das pecas######################//
+    int config = 2;
+    int *pecas = leitura (nome_arquivo, config);
+    // for (int i = 0; *(pecas+i) != '\0'; i++) printf("%c",*(pecas+i));
+    // printf("\n");
+    //######################coleta do local das pecas######################//
+   
+    
+    return 0;
 }
 
 char *nome_txt(int argc, char *argv[]){
+
     int c,count_letter, anterior, flag_name, contem_f;
     flag_name = contem_f = FALSE;
     count_letter = anterior = 0;
@@ -52,4 +63,43 @@ char *nome_txt(int argc, char *argv[]){
         p = nome_arquivo_array;
     }
     return p;
+}
+
+int *leitura (char *nome_arquivo, int config){
+
+  FILE *pont_arq;
+  int count = 0, contador = 0, c;
+  int flag_wall = FALSE;
+  static int array[100];
+  
+      
+  pont_arq = fopen(nome_arquivo,"r");
+  
+  do
+  {  
+      c = fgetc(pont_arq);
+      if (config == 1){
+        if (c == '*') flag_wall = TRUE;
+        if (flag_wall == TRUE && count < 42 && c != '\n') array[count++] = c;
+      }
+      else{
+        if (c == '*' && contador > 20) flag_wall = TRUE;
+        if (flag_wall == TRUE && count < 55 && c != '\n') array[count++] = c;
+      }
+      if (c =='*')contador++;
+  }while (c != EOF);
+  
+  fclose(pont_arq);//fechando o arquivo
+
+  array[count] = '\0';
+
+  int *p = array;
+
+  // int i= 0;
+  // for (; array[i] != '\0'; i++){
+  //     printf("%c",array[i]);
+  // }
+  // printf("\n");
+
+  return p;
 }
